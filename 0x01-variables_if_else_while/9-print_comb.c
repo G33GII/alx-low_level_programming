6af952d61764f5955ff521b127e624fbcb3a4179
shekwogaza <gazasolomon@gmal.com>
2023-08-11 21:54:35 +0000

    int lett = D and putchar(lett)

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/* Headers - go here at the start of the code */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* main - funtion starts wit int main void here */
+/**
+ *main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int s_d = 0;
+	int lett;
+
+	while (s_d < 10)
+	{
+		putchar(48 + s_d);
+		lett = 44;
+		putchar(lett);
+		lett = ' ';
+		putchar(lett);
+		s_d++;
+	}
+	putchar('\n');
+
+return (0);
+}
