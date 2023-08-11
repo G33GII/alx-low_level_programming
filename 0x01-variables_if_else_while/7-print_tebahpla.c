cb2236f4c730dbfd9f5ce856b428f6e069ae60a0
shekwogaza <gazasolomon@gmal.com>
2023-08-11 19:51:02 +0000

    program that prints the lowercase alphabet in reverse, followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/* Head section */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 
+ * Return: 0
+ */
+int main(void)
+{
+	/* main body of code */
+	char alp = 122;
+	while (alp > 96)
+	{
+		putchar(alp);
+		alp--;
+	}
+return (0);
+}
