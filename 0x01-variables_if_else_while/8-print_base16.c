11c550537797893005bee66da04f52f46f224988
shekwogaza <gazasolomon@gmal.com>
2023-08-11 20:59:50 +0000

    a program that prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more - headers goes there */
+/* Return: 0 */
+/* betty style doc for function main goes there */
+
+/* main - funtion starts wit int main void here */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+
+
+
+
+
+
+	int hex_alp = 97;
+	int ch_a = 0;
+
+	for (ch_a = 0; ch_a < 10; ch_a++)
+	{
+		putchar(48 + ch_a);
+	}
+	while (hex_alp < 103)
+	{
+		putchar(hex_alp);
+		hex_alp++;
+	}
+	putchar('\n');
+	return (0);
+}
