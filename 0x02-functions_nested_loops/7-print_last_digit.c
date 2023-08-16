5a701a474d15b25540ea08c5982c59a1ddaa7805
shekwogaza <gazasolomon@gmal.com>
2023-08-16 01:01:51 +0000

    @7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - funtion defination that does the magic
+ * @gh: input value to the functioin
+ * Return: 1 or 0
+ * -1 for negatives
+ *  0 for equal to 0
+ *
+ */
+
+int print_last_digit(int gh)
+{
+	int gaza;
+
+	if (gh < 0)
+		gh = -gh;
+
+	gaza = gh % 10;
+
+	if (gaza < 0)
+		gaza = -gaza;
+
+	_putchar(gaza + '0');
+	return (gaza);
+}
+
