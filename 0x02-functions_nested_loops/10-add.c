396fad32d4e1ebd5fba72a33c823af57d703c8b4
shekwogaza <gazasolomon@gmal.com>
2023-08-16 01:46:17 +0000

    @10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - funtion defination that does the magic
+ * gaza: input value to the functioin
+ * sarah: input value
+ * Return: 1 or 0
+ * -1 for negatives
+ *  0 for equal to 0
+ *
+ */
+
+int add(int gaza, int sarah)
+{
+	int gasa = gaza + sarah;
+	return (gasa);
+}
