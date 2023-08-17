9fd7ea22010b040f6d2acdad55cb0d3373b1fd00
shekwogaza <gazasolomon@gmal.com>
2023-08-17 00:02:41 +0000

    @100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,28 +21,28 @@ void print_times_table(int g)
 					putchar(product + '0');
 				} else if (product < 10 && col != 0)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					putchar(product + '0');
 				} else if (product >= 10 && product < 100)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar((product / 10) + '0');
-					putchar((product % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
 				} else if (product >= 100)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar((product / 100) + '0');
-					putchar(((product / 10) % 10) + '0');
-					putchar((product % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
 				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
