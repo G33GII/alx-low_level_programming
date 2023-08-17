4f45af91f140b8d15a5bff740125c7bf752432a3
shekwogaza <gazasolomon@gmal.com>
2023-08-17 19:21:20 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - funtion defination that does the magic
+ * @void: no input value to the functioin only a std output
+ * Return: 1 or 0
+ *
+ */
+
+void more_numbers(void)
+{
+	int c = 0;
+	int b;
+
+	while (c < 9)
+	{
+		b = 0;
+
+		while (b < 15)
+		{
+			if (b >= 10 && b < 19)
+				_putchar (b / 10 + 48);
+			_putchar (b % 10 + 48);
+			b++;
+		}
+		_putchar('\n');
+		c++;
+	}
+	_putchar('\n');
+}
