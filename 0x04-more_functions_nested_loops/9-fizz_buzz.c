00a1a29fe7f6e6ad0b7aa8a098c433dff386773b
shekwogaza <gazasolomon@gmal.com>
2023-08-18 03:32:05 +0000

    @9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,43 +3,35 @@
 
 /**
 * main - followed by a new line
-* fizzbuzz: multiples of three prints fizz buzz instead of the number
+* fizzbuzz multiples of three prints fizz buzz instead of the number
 * @fizzbuzzcollects nothing and displays everything
 * Return: Always 0 (Success)
 */
 
-void fizzbuzz(void);
+
 
 int main(void)
 {
-	fizzbuzz();
-	return (0);
-}
+	int ic;
 
-
-void fizzbuzz(void)
-{
-	int i;
-
-	for (i = 1; i <= 100; i++)
+	for (ic = 1; ic <= 100; ic++)
 	{
-
-		if (i % 3 == 0)
+		if (ic % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if (ic % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 15 == 0)
+		else if (ic % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
 		else
-			printf("%d ", i);
+			printf("%d ", ic);
 	}
 
 	printf("\n");
+	return (0);
 }
-
