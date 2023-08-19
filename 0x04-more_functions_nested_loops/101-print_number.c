6d43b9207e9b380417c25764ac0becbc80b1e104
Shekwogaza <gazasolomon@gmail.com>
2023-08-19 11:35:03 -0700

    updated 101 print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * main - function
+ * print_number - function
+ * @n: input variable
  * Return: 0 always
  *
  */
 void print_number(int n)
 {
+	int reversed;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n > 0)
+	{
+		reversed = reversed * 10 + (n % 10);
+		n /= 10;
+	}
 
-
-
+	if (reversed == 0)
+	{
+		_putchar('0');
+	} else
+	{
+		while (reversed > 0)
+		{
+			_putchar('0' + (reversed % 10));
+			reversed /= 10;
+		}
+	}
 }
