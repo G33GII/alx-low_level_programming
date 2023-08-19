81bbe58439f2d27a49171f0bd9b74c7761df2fbe
Shekwogaza <gazasolomon@gmail.com>
2023-08-19 09:26:50 -0700

    some files am nit sure about

--- 0x02-functions_nested_loops/11/100-prime_factor.c
+++ 0x02-functions_nested_loops/11/100-prime_factor.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_triangle - print triangle
- * @size: size of triangle
- * Return: 0
- */
+ *  * print_triangle - print triangle
+ *   * @size: size of triangle
+ *    * Return: 0
+ *     */
 void print_triangle(int size)
 {
 	if (size <= 0)
