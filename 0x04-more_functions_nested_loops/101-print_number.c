27fb714eaab346d5e13b0f0122ef7032c62f3189
Shekwogaza <gazasolomon@gmail.com>
2023-08-19 10:59:32 -0700

    created 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0 always
+ *
+ */
+int main(void)
+{
+	long num = 612852475143;
+	long factor = 2;
+
+	while (num > 1)
+	{
+		if (num % factor == 0)
+		{
+			num = num / factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+	printf("%li\n", factor);
+
+	return (0);
+}
