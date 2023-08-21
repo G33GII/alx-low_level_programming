5af9d6a232209d64b90bc1e4c023bbfb7a83f8c5
shekwogaza <gazasolomon@gmal.com>
2023-08-21 17:27:10 +0000

    updated a function that prints a string, followed by a new line, to stdout. @3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - 
- * @str: 
- * Retruns void
- * 
+ * _puts - literally puts text on the std output
+ * @str: pointer with a vlue of a string
+ * Retruns void. std output.
+ *
  *
  */
 void _puts(char *str)
 {
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
 
-	
+
 }
