83cf71395f46e418480199de8491a5292d0e7b14
shekwogaza <gazasolomon@gmal.com>
2023-08-22 07:17:14 +0000

    update on description messgae in tasks 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - funtion defination that does the magic
- * @n: no input value to the functioin only a std output
+ * reset_to_98 - A funtion that takes a pointer to an int as parameter and updates the value it points to to 98
+ * @n: input value to the functioin where *n = &b;
  * Return: 0 always.
  *
  */
