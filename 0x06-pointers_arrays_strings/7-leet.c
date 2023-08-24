339218891537d2227b679fccbb63abfe30159bb8
shekwogaza <gazasolomon@gmal.com>
2023-08-24 21:56:54 +0000

    update on @7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+ /**
+  * leet - Translates a string to leetspeak
+  * @str: The string to be translated.
+  *
+  * This function converts characters
+  * in the given string to their leetspeak equivalents.
+  *
+  * It modifies the original string
+  * and
+  *
+  * Return: a pointer to the modified string.
+  */
+
+char *leet(char *str)
+{
+	int i, j;
+	char g[] = "aAeEoOlLtT"; /* Original characters to be replaced */
+	char f[] = "4433007711"; /* Leet equivalents */
+
+	/* Iterate through each character in the input string */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		/* Iterate through each character pair in g and f */
+		for (j = 0; g[j] != '\0'; j++)
+		{
+			/* If the current character in the input */
+			 /* matches a character in g */
+			if (str[i] == g[j])
+			{
+				str[i] = f[j]; /* Replace the character with */
+						/*  the corresponding leet equivalent */
+			}
+		}
+	}
+
+	return (str); /* Return the modified string */
+}
