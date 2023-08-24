e820f5afaefecffa707ab1806c892e6d81e8b899
shekwogaza <gazasolomon@gmal.com>
2023-08-24 16:17:55 +0000

    update @6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
  * cap_string - Capitalizes the first letter of each word in a string
  * @str: The string to be modified.
  *
  * This function capitalizes the first letter of each word in the given string.
- * It modifies the original string and returns a pointer to the modified string.
+ * It modifies the original string and
+ * Return: a pointer to the modified string.
  */
+
 char *cap_string(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		/* Capitalize the first letter after specified delimiters or at the beginning */
+		/* Capitalize the first letter after specified delimiters */
 		if (i == 0 ||
 				str[i - 1] == ' ' ||
 				str[i - 1] == '\t' ||
@@ -39,4 +42,3 @@ char *cap_string(char *str)
 
 	return (str);
 }
-
