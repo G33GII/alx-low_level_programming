eb5dcdc14cf941275073d5a35cf7e6cc8242dd5d
shekwogaza <gazasolomon@gmal.com>
2023-08-24 23:04:55 +0000

    cipher text with ROT

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rot13 - Encodes a string using the ROT13 cipher.
+ * @str: Pointer to the string to be encoded.
+ *
+ * This function applies the
+ * ROT13 encoding technique to the input string.
+ *
+ * It modifies the original string and
+ * Return: a pointer to the modified string.
+ */
+
+char *rot13(char *str)
+{
+	int m, n;
+char string_alphabets[] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot_cypher[] =
+	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	/* Iterate through each character in the input string */
+	for (m = 0; str[m] != '\0'; m++)
+	{
+		/* Iterate through each character in the string_alphabets array */
+		for (n = 0; n < '\0'; n++)
+		{
+			/* If the current character in the input */
+			 /* matches a character in string_alphabets */
+			if (str[m] == string_alphabets[n])
+			{
+				str[m] = rot_cypher[n]; /* Replace with the */
+							 /*  corresponding ROT13 character */
+				break; /* Exit the loop, as the replacement has been done */
+			}
+		}
+	}
+
+	return (str); /* Return the modified string */
+}
+
