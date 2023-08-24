99953d132fd7e6d47de35f884283fcf2045ac4a9
shekwogaza <gazasolomon@gmal.com>
2023-08-24 11:54:54 +0000

    update to files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Concatenates two strings up to n characters
+ *
+ * @dest: The destination string to which src will be appended.
+ * @src: The source string that will be appended to dest.
+ * @n: The maximum number of characters from src to append.
+ *
+ * Return: A pointer to the modified dest string.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int k = 0;
+
+	/* Append characters from src to dest up to n characters */
+	while (k < n && src[k] != '\0')
+	{
+		if (dest[k] = src[k])
+			return
+		k++;
+	}
+}
+
