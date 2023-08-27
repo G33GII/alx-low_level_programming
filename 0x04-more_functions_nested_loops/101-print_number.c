75dcc243819844a8398fd1fb3ff5f5e80cdb182e
G33GII <gazasolomon@gmail.com>
2023-08-27 17:47:45 +0100

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,30 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * print_number - main function
- * @n: funtion input
- */
+#include "main.h"    /* Include necessary header file */
+#include <stdio.h>   /* Include standard I/O library */
 
+/**
+ * print_number - Recursively prints an integer.
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
-	unsigned int num;
+    unsigned int num;
 
-	if (n < 0)
-	{
-		num = -n;
-		_putchar('-');
-	}
-	else
-	{
-		num = n;
-	}
+    /* If the number is negative, convert it to positive and print a '-' sign */
+    if (n < 0)
+    {
+        num = -n;      /* Make the number positive */
+        _putchar('-'); /* Print '-' for negative numbers */
+    }
+    else
+    {
+        num = n;       /* Use the number as is */
+    }
 
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
+    /* Recursive call to print each digit of the number */
+    if (num / 10)
+    {
+        print_number(num / 10);
+    }
 
-	_putchar((num % 10) + '0');
-
+    _putchar((num % 10) + '0'); /* Print the last digit of the number */
 }
