4855197dff818106182a2440210fddfa58037d41
Shekwogaza <gazasolomon@gmail.com>
2023-08-28 21:02:17 +0100

    uppdate

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,21 +2,39 @@
 #include "main.h"
 
 /**
- * _strchr - Locate the first occurrence of a character in a string.
+ * _strspn - Calculate the length of the initial segment of a string
+ *           consisting only of characters from a given set.
  * @s: Pointer to the string to search.
- * @c: Character to search for.
+ * @accept: Pointer to the set of characters to accept.
  *
- * Return: Pointer to the first
- * occurrence of the character, or NULL if not found.
+ * Return: Length of the initial segment of
+ * @s containing only characters
+ *         from @accept.
  */
-char *_strchr(char *s, char c)
-{
-	int f; /* Loop counter */
+unsigned int _strspn(char *s, char *accept)
+{	/* Length of the matching segment */
+	unsigned int segment_length = 0, j, i;
 
-	for (f = 0; s[f] != '\0'; f++)
-	{
-		if (s[f] == c)
-			return (s + f); /* Return pointer to the found character */
+	/* Iterate through the input string */
+	for (i = 0; s[i] != '\0'; i++)
+	{	/* Flag to track character match */
+		int char_found = 0;
+		/* Iterate through the accept set */
+		for (j = 0; accept[j] != '\0'; j++)
+		{	/* Check if current character matches */
+			if (s[i] == accept[j])
+			{	/* Mark character as found in the accept set */
+				char_found = 1;
+				break; /* Exit the loop once a match is found */
+			}
+		}
+		if (char_found == 0) /* If character not found in accept set */
+		{	/* Return the length of the matching segment */
+			return (segment_length);
+		}
+	/* Increment the count for each matching character */
+segment_length++;
 	}
-	return (0); /* Character not found, return NULL */
+	/* Return the length of the entire string if all match */
+	return (segment_length);
 }
