39c7234f13153f9dbb680efcc9ab5c86453715fd
Shekwogaza <gazasolomon@gmail.com>
2023-08-30 00:09:08 +0100

    update on @8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,34 +8,19 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int sSum = 0;
+	int tSum = 0;
 	int i;
-	int j;
-	int s = 0, t = 0;
-	int matrix[size][size]; /* Declare a 2D matrix of size x size */
 
-	/* Populate the matrix using the provided 1D array */
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{	/* Calculate the index and assign value */
-			matrix[i][j] = *(a + i * size + j);
-		}
-	}
+		/* Calculate the indices for the main diagonal and anti-diagonal */
+		int sIndex = i * size + i;
+		int tIndex = (i + 1) * size - (i + 1);
 
-	/* Calculate the sum of diagonal elements */
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j) /* Check for main diagonal */
-			{	/* Add the main diagonal element to 's' */
-				s += matrix[i][j];
-			}
-			if (j == size - 1 - i) /* Check for secondary diagonal */
-			{	/* Add the secondary diagonal element to 't' */
-				t += matrix[i][j];
-			}
-		}
+		/* Add elements to the respective sums */
+		sSum += a[sIndex];
+		tSum += a[tIndex];
 	}
-	printf("%d, %d\n", s, t); /* Print the sum of diagonal elements */
+	printf("%d, %d\n", sSum, tSum);
 }
