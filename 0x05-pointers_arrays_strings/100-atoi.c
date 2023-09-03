71fa308d3d13d86928614510e980e613a68a4a5e
Shekwogaza <gazasolomon@gmail.com>
2023-09-04 00:19:37 +0100

    new file:   0x05-pointers_arrays_strings/100-atoi.c modified:   0x05-pointers_arrays_strings/a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _atoi - function name
+* 
+* @s: source
+* Return: an integer.
+*/
+int _atoi(char *s)
+{
+	unsigned int number = 0, n = 1;
+
+	while (!(*s >= '0' && *s <= '9') && *s != '\0')
+	{
+		if (*s == '-')
+			n = -1;
+		else if (*s == ' ' || *s == '\t' || *s == '+')
+			n = 1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9' && *s != '\0')
+	{
+		number = (number * 10) + (*s - '0');
+		s++;
+	}
+
+	if (number == 0)
+		return (0);
+	
+	return (number * n);
+}
