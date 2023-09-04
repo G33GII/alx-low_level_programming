48c5c88da1b570e290e965d24ede20dcb2e1c021
Shekwogaza <gazasolomon@gmail.com>
2023-09-04 18:12:12 +0100

    modified:   0x0B-malloc_free/a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,57 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * str_concat - main functiin
- * @s1: first input
- * @s2: second input
- * Return: pointer to a new string
+ * str_concat - Concatenates two strings s1 and s2, returns a new string
+ * @s1: The first string to be concatenated, can be NULL.
+ * @s2: The second string to be concatenated, can be NULL.
+ *
+ * This function calculates the length of the resulting string,
+ * allocates memory for it,
+ * and copies characters from s1 and s2 to create the concatenated string.
+ *
+ * Return: A pointer to the concatenated string
+ * (newly allocated) or NULL if allocation fails.
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	size_t idx, idz, z;
+	size_t idx, idz = 0, z;
 	char *sx;
 
-	idx = strlen(s1) + strlen(s2) + 1;
-
+	/* Determine the length of the resulting string */
+	if (s1 == NULL && s2 == NULL)
+		idx = 1;
+	else if (s1 != NULL && s2 != NULL)
+		idx = strlen(s1) + strlen(s2) + 1;
+	else if (s1 == NULL && s2 != NULL)
+		idx = strlen(s2) + 1;
+	else if (s1 != NULL && s2 == NULL)
+		idx = strlen(s1) + 1;
+	/* Allocate memory for the resulting string */
 	sx = malloc(idx * sizeof(char));
-
-	if (s1 == NULL)
-		sx = strlen;
-	else
+	/* Copy characters from s1 to the result, if s1 is not NULL */
+	if (s1 != NULL)
 	{
-
-		for (idz = 0; idz < strlen(s1); idz++)
+		for (idz = 0; idz < idx; idz++)
 		{
+			if (s1[idz] == '\0')
+				break;
 			sx[idz] = s1[idz];
 		}
-		for (z = 0; idz < idx; idz++, z++)
+	}
+	/* Copy characters from s2 to the result, if s2 is not NULL */
+	if (s2 != NULL)
+	{
+		for (z = 0; z < idx; idz++, z++)
 		{
+			if (s2[z] == '\0')
+				break;
 			sx[idz] = s2[z];
 		}
-		sx[idz] = '\0';
 	}
+	/* Null-terminate the resulting string */
+	sx[idz] = '\0';
+	/* Return the concatenated string */
 	return (sx);
-
-
 }
