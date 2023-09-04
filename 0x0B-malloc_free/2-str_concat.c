c37513ffaf094fd0f0df5cb89c291351fe8f9fb3
Shekwogaza <gazasolomon@gmail.com>
2023-09-04 19:49:58 +0100

    new file:   0x0B-malloc_free/txt.txt

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,18 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "main.h"
 /**
  * str_concat - Concatenates two strings s1 and s2, returns a new string
  * @s1: The first string to be concatenated, can be NULL.
  * @s2: The second string to be concatenated, can be NULL.
- *
- * This function calculates the length of the resulting string,
- * allocates memory for it,
- * and copies characters from s1 and s2 to create the concatenated string.
- *
  * Return: A pointer to the concatenated string
- * (newly allocated) or NULL if allocation fails.
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -21,20 +12,18 @@ char *str_concat(char *s1, char *s2)
 
 	/* Determine the length of the resulting string */
 	if (s1 == NULL && s2 == NULL)
-	 length = 1;
+		length = 1;
 	else if (s1 != NULL && s2 != NULL)
-	 length = strlen(s1) + strlen(s2) + 1;
+		length = strlen(s1) + strlen(s2) + 1;
 	else if (s1 == NULL && s2 != NULL)
-	 length = strlen(s2) + 1;
+		length = strlen(s2) + 1;
 	else if (s1 != NULL && s2 == NULL)
-	 length = strlen(s1) + 1;
+		length = strlen(s1) + 1;
 	/* Allocate memory for the resulting string */
 	New_str = malloc(length * sizeof(char));
 	/* Copy characters from s1 to the result, if s1 is not NULL */
-
 	if (New_str == NULL)
-        return (0);
-
+		return (0);
 	if (s1 != NULL)
 	{
 		for (index = 0; index < length; index++)
@@ -43,8 +32,7 @@ char *str_concat(char *s1, char *s2)
 				break;
 			New_str[index] = s1[index];
 		}
-	}
-	/* Copy characters from s2 to the result, if s2 is not NULL */
+	}	/* Copy characters from s2 to the result, if s2 is not NULL */
 	if (s2 != NULL)
 	{
 		for (z = 0; z < length; index++, z++)
@@ -54,8 +42,6 @@ char *str_concat(char *s1, char *s2)
 			New_str[index] = s2[z];
 		}
 	}
-	/* Null-terminate the resulting string */
-	New_str[index] = '\0';
-	/* Return the concatenated string */
-	return (New_str);
+	New_str[index] = '\0';/* Null-terminate the resulting string */
+	return (New_str);/* Return the concatenated string */
 }
