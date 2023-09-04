ef722db8a66c1892ee4f36cbe97a1ee9ba04921b
Shekwogaza <gazasolomon@gmail.com>
2023-09-04 04:58:47 -0700

    uodate @create-arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,10 +21,10 @@ char *create_array(unsigned int size, char c)
 	if (a == NULL)
 		return (0);
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
 	}
-
 	return (a);
+	free(a);
 }
