a1dff6c8110d2f87025f04d3e8f8fc2266d28860
Shekwogaza <gazasolomon@gmail.com>
2023-09-04 06:22:46 -0700

    update realloc demo

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - function
+ *
+ * @str: input
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *b;
+	size_t idx, m;
+
+	idx = strlen(str);
+
+	b = malloc(idx * sizeof(char));
+	if (b == NULL)
+		return (0);
+	for (m = 0; m < idx; m++)
+	{
+		b[m] = str[m];
+	}
+	if (strlen(b) != idx)
+		return (0);
+
+	return (b);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,8 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 #endif
