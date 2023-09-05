569aa1557314a3d616aa3e79e75ac506aacdd6b4
Shekwogaza <gazasolomon@gmail.com>
2023-09-05 16:50:06 +0100

    modified:   0x0B-malloc_free/4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,10 +9,10 @@
 
 void free_grid(int **grid, int height)
 {
-    int x;
+	int x;
 
-    for (x = 0; x < height; x++)
-        free(grid[x]); /* Free memory for each row of the 2D array */
-    
-    free(grid); /* Free the memory for the array of row pointers */
+	for (x = 0; x < height; x++)
+		free(grid[x]); /* Free memory for each row of the 2D array */
+
+	free(grid); /* Free the memory for the array of row pointers */
 }
