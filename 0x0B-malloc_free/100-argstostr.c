a20b9f489055d7d8985734b94be91ba1cad22528
Shekwogaza <gazasolomon@gmail.com>
2023-09-05 18:04:36 +0100

    modified:   0x0B-malloc_free/main.h

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - Concatenate an array of strings into a single string with each
+ * string on a new line.
+ * @ac: The number of strings in av.
+ * @av: An array of strings.
+ * Return: A pointer to the concatenated string, or NULL on failure.
+ */
+char *argstostr(int ac, char **av)
+{
+	int x, y, total_length = 0;
+	char *result;
+	char *current;
+
+	if (ac == 0 || av == NULL)
+	{
+		/* Return NULL for invalid input */
+		return (NULL);
+	}
+	/* The total length of all strings, including newlines & null terminators */
+	for (x = 0; x < ac; x++)
+	{
+		if (av[x] == NULL)
+		{
+			/* Return NULL if any of the strings are NULL */
+			return (NULL);
+		}
+		total_length += strlen(av[x]) + 1; /* +1 for the newline character */
+	}
+	/* Allocate memory for the concatenated string */
+	result = malloc(total_length * sizeof(char));
+	if (result == NULL)
+	{
+		/* Return NULL if memory allocation fails */
+		return (NULL);
+	}
+	current = result;
+	/* Copy each string from av into the result string with a newline character */
+	for (x = 0; x < ac; x++)
+	{
+		y = strlen(av[x]);
+		strncpy(current, av[x], y);
+		current += y;
+		*current = '\n'; /* Add a newline character between concatenated strings */
+		current++;
+	}
+	*(current - 1) = '\0'; /* Replace the last newline with a null terminator */
+	return (result);
+}
