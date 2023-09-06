f08afcde37d71fe25242a8bcda327bdd6acf0f08
Shekwogaza <gazasolomon@gmail.com>
2023-09-06 23:42:37 +0100

    0x0B-malloc_free/strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * strtow - 
+ * @str: 
+ * 
+ * Return: 
+ * 
+*/
+
+char **strtow(char *str)
+{                                                                                                   /* "      ALX School         #cisfun      " */
+    char **rz;                                                                                       /* int** arr = (int**)malloc(r * sizeof(int*)); */   
+    int x, z, xl;                                                                                      /* for (i = 0; i < r; i++) */ 
+                                                                                                        /* arr[i] = (int*)malloc(c * sizeof(int)); */
+    if (!str || str == "")
+        return (NULL);
+
+
+    
+
+
+
+
+
+
+
+}
