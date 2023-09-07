04dfc51129ea2806b32c4926b53500cf2da9515b
Shekwogaza <gazasolomon@gmail.com>
2023-09-07 01:20:22 +0100

    modified:   0x0B-malloc_free/101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,28 +1,55 @@
 #include "main.h"
 
 /**
- * strtow - 
- * @str: 
- * 
- * Return: 
- * 
-*/
+ * word_count - Counts the number of words in a string.
+ * @s: The input string.
+ *
+ * Return: The number of words in the string.
+ */
 
+int word_count(char *s)
+{
+	int wx = 0;    /* Word count */
+	int flag = 0;  /* Flag to track word boundaries */
+
+	for (int x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] == ' ')
+		{
+			flag = 0; /* Reset flag when encountering a space */
+		}
+		else if (flag == 0)
+		{
+			flag = 1; /* Set flag to 1 when starting a new word */
+			wx++;     /* Increment word count */
+		}
+	}
+
+	return (wx);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: An array of strings containing the words.
+ */
 char **strtow(char *str)
-{                                                                                                   /* "      ALX School         #cisfun      " */
-    char **rz;                                                                                       /* int** arr = (int**)malloc(r * sizeof(int*)); */   
-    int x, z, xl;                                                                                      /* for (i = 0; i < r; i++) */ 
-                                                                                                        /* arr[i] = (int*)malloc(c * sizeof(int)); */
-    if (!str || str == "")
-        return (NULL);
+{
+	int i = 0;
 
+	char *str_copy = strdup(str);         /* Make a copy of the string */
+	int x = word_count(str_copy);         /* Count words in the copy */
 
-    
+	char **pp = malloc(sizeof(char *) * x); /* Allocate memory for the words */
+	char *ttx = strtok(str_copy, " ");    /* Tokenize the copy */
 
+	while (ttx != NULL)
+	{
+		pp[i] = strdup(ttx); /* Make a copy of the token */
+		ttx = strtok(NULL, " ");
+		i++;
+	}
 
-
-
-
-
-
+	return (pp);
 }
