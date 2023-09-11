9c9f22123f521ab4877c6f7942b2480fbd368acd
Shekwogaza <gazasolomon@gmail.com>
2023-09-11 19:29:31 +0100

    new file:   0x0D-preprocessor/1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,3 @@
+#ifndef PI  /* If PI is not defined */
+#define PI 3.14159265359    /* define it as the mathematical constant Pi */
+#endif  /* This constant represents an approximation of Pi for calculations. */
