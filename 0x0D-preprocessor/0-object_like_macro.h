c7a1e5ed0d0ad8d6e4c7ed5f25d564364b305e22
Shekwogaza <gazasolomon@gmail.com>
2023-09-11 19:23:10 +0100

    modified:   0x0D-preprocessor/0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,3 @@
 #ifndef SIZE
-/**
- * If SIZE is not defined, define it as 1024.
- * This constant represents a default size.
-*/
-#define SIZE (1024)
-#endif
+#define SIZE (1024) /* If SIZE is not defined, define it as 1024. */
+#endif  /*This constant represents a default size. */
