601efd9822ebf203beb63a7444a004300f3a7ae1
Shekwogaza <gazasolomon@gmail.com>
2023-09-13 23:54:22 +0100

    @Advance

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,10 +9,11 @@
  *
  * Return: 0 if successful.
  */
+
 int main(int argc, char *argv[])
 {
 	int x_byt;
-	int x = 0;
+	int z, x = 0;
 	char *x_rr;
 
 	if (argc != 2)
@@ -38,7 +39,7 @@ int main(int argc, char *argv[])
 			printf("%02hhx\n", x_rr[x]);
 			break;
 		}
-		printf("%02hhx", x_rr[x]);
+		printf("%2hhx", x_rr[x]);
 		x++;
 	}
 
