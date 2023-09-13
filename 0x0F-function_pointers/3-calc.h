7ca81f4a4d112e0a6e923c1c2066ce547d54e1df
Shekwogaza <gazasolomon@gmail.com>
2023-09-13 20:05:17 +0100

    modified:   0x0F-function_pointers/3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -5,52 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 
-
 /**
- * op_add - Adds two integers.
- * @a: input value.
- * @b: input value.
- * Return: integer.
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 int op_add(int a, int b);
-
-/**
- * op_sub - Subtracts two integers.
- * @a: input value.
- * @b: input value.
- * Return: integer.
- */
 int op_sub(int a, int b);
-
-/**
- * op_mul - Multiplies two integers.
- * @a: input value.
- * @b: input value.
- * Return: integer.
- */
 int op_mul(int a, int b);
-
-/**
- * op_div - Divides two integers.
- * @a: input value.
- * @b: input value.
- * Return: integer.
- */
 int op_div(int a, int b);
-
-/**
- * op_mod - Computes the remainder of integer division.
- * @a: input value.
- * @b: input value.
- * Return: integer.
- */
 int op_mod(int a, int b);
-
-/**
- * get_op_func - selects the correct function to perform the operation.
- * s The operator passed as an argument to the program.
- * Return: integer function pointer.
- */
 int (*get_op_func(char *s))(int, int);
 
 #endif
