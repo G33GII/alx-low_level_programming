9fcc07efedcc0b8b316540a34bbb479176520d1b
Shekwogaza <gazasolomon@gmail.com>
2023-09-14 23:56:17 +0100

    update at 3 print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,65 +1,54 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
  * print_all - Print formatted data based on a format string.
- * @format: A format string containing type specifiers.
+ * @format: A format string containing type specifiers:
+ *           - 'c' for character
+ *           - 'i' for integer
+ *           - 'f' for float
+ *           - 's' for string
  */
-void print_all(const char *const format, ...)
+
+void print_all(const char * const format, ...)
 {
-    char *z_strings;
-    int x = 0;
-    va_list x_list;
-    int z_printed = 0; /* Track if anything has been printed. */
+	char *z_strings, *x_seperator = "";
+	int x = 0;
 
-    va_start(x_list, format);
+	va_list x_list;
 
-    if (format)
-    {
-        while (format[x] != '\0')
-        {
-            switch (format[x])
-            {
-                case 'c':
-                    printf("%c", va_arg(x_list, int));
-                    z_printed = 1; /* Mark that an argument was printed. */
-                    break;
+	va_start(x_list, format);
 
-                case 'i':
-                    printf("%d", va_arg(x_list, int));
-                    z_printed = 1;
-                    break;
+	if (format)
+	{
+		while (format[x])
+		{
+			switch (format[x])
+			{
+				case 'c':
+					printf("%s%c", x_seperator, va_arg(x_list, int));
+					break;
+				case 'i':
+					printf("%s%d", x_seperator, va_arg(x_list, int));
+					break;
+				case 'f':
+					printf("%s%f", x_seperator, va_arg(x_list, double));
+					break;
+				case 's':
+					z_strings = va_arg(x_list, char*);
+					printf("%s%s", x_seperator, z_strings);
+					if (z_strings == NULL)
+						printf("%s%s", x_seperator, "(nil)");
+					break;
+				default:
+					break;
+			}
+			x_seperator = ", ";
+			x++;
+		}
+	}
 
-                case 'f':
-                    printf("%f", va_arg(x_list, double));
-                    z_printed = 1;
-                    break;
-
-                case 's':
-                    {
-                        z_strings = va_arg(x_list, char *);
-                        if (z_strings == NULL)
-                        {
-                            printf("(nil)");
-                            break;
-                        }
-                        printf("%s", z_strings);
-                        z_printed = 1;
-                        break;
-                    }
-
-                default:
-                    break;
-            }
-
-            if (format[x + 1] != '\0' && z_printed) /* Print comma if an argument was printed. */
-                printf(", ");
-
-            x++;
-            z_printed = 0; /* Reset the printed flag. */
-        }
-    }
-
-    printf("\n");
-    va_end(x_list);
+	printf("\n");
+	va_end(x_list);
 }
