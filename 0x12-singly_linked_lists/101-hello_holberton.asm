a21f1be4400425bbbd9237a6f99a6eaccfbbc19e
G33GII <gazasolomon@gmail.com>
2023-09-22 00:48:29 +0100

    Removed comments from 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,11 @@
-global main              /* Declare the entry point of the program as "main" */
-extern printf            /* Declare the use of the "printf" function */
+global main
+extern printf
 
 section .text
-main:                    /* Start of the main function */
-    mov edi, format      /* Load the address of the format string into edi */
-    xor eax, eax         /* Clear the eax register (set it to 0) */
-    call printf          /* Call the printf function to print the message */
-    mov eax, 0           /* Set the return value to 0 (success) */
-    ret                  /* Return from the main function */
-    format db `Hello, Holberton\n`,0  /* Define a null-terminated string */
-    
+main:
+    mov edi, format
+    xor eax, eax
+    call printf
+    mov eax, 0
+    ret
+    format db `Hello, Holberton\n`,0
