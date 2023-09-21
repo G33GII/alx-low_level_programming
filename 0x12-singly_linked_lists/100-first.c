85359f92ffb5a554ee57d49d498d0097df3d0602
G33GII <gazasolomon@gmail.com>
2023-09-22 00:25:57 +0100

    modified:   0x12-singly_linked_lists/100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,6 @@
 void first(void) __attribute__ ((constructor));
 /**
  * first - Print two lines of text to the standard output.
- * 
  * This function prints the following two lines:
  * 1. "You're beat! and yet, you must allow,"
  * 2. "I bore my house upon my back!"
@@ -11,7 +10,7 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 
 }
