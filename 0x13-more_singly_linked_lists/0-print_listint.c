d75bd70215eacf8bd81aa56596657a27b53d730a
G33GII <gazasolomon@gmail.com>
2023-09-26 11:07:21 +0100

    canges to print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,22 +10,16 @@ size_t print_listint(const listint_t *h)
 	/* Initialize node count */
 	size_t node_count = 0;
 
-	/* Check if the list is empty */
-	if (h == NULL)
-	{
-		return (node_count);
-	}
-
 	/* Traverse through the linked list */
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 
-		/* Increment the node count */
-		node_count++;
-
 		/* Move to the next node */
 		h = h->next;
+
+		/* Increment the node count */
+		node_count++;
 	}
 
 	/* Return the node count */
