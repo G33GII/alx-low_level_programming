f4fa2e78f957ae803efe2d8c35e98d64e5b44d34
G33GII <gazasolomon@gmail.com>
2023-09-26 14:28:48 +0100

    reverse listint

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,22 +7,22 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev_node = NULL; /* Pointer to the previous node */
-    listint_t *nxt_node = NULL; /* Pointer to the nxt node */
+	listint_t *prev_node = NULL; /* Pointer to the previous node */
+	listint_t *nxt_node = NULL; /* Pointer to the nxt node */
 
-    if (*head == NULL)
-    {
-        return (NULL); /* Return NULL if the list is empty */
-    }
+	if (*head == NULL)
+	{
+		return (NULL); /* Return NULL if the list is empty */
+	}
 
-    while (*head != NULL)
-    {
-        nxt_node = (*head)->next; /* Store the nxt_node node */
-        (*head)->next = prev_node; /* Reverse the nxt pointer */
-        prev_node = *head;        /* Move prev to the current node */
-        *head = nxt_node;        /* Move head to the nxt node */
-    }
+	while (*head != NULL)
+	{
+		nxt_node = (*head)->next; /* Store the nxt_node node */
+		(*head)->next = prev_node; /* Reverse the nxt pointer */
+		prev_node = *head;        /* Move prev to the current node */
+		*head = nxt_node;        /* Move head to the nxt node */
+	}
 
-    *head = prev_node; /* Update the head to the new head of the reversed list */
-    return (*head);
+	*head = prev_node; /* Update the head to the new head of the reversed list */
+	return (*head);
 }
