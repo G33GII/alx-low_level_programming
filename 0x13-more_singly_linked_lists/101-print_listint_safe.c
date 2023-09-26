cf6a86d4888f63d39a3adc7bec3a0b06607190ed
G33GII <gazasolomon@gmail.com>
2023-09-26 15:19:30 +0100

    new file:   0x13-more_singly_linked_lists/101-main.c 	new file:   0x13-more_singly_linked_lists/101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints the elements of a listint_t list safely.
+ * @head: A pointer to the head of the list.
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+    size_t node_count = 0; /* Initialize a counter for the number of nodes */
+    const listint_t *current = head; /* Create a current pointer for traversal */
+
+    /* Check if the list is empty */
+    if (!head)
+    
+    {
+        exit(98); /* Exit with code 98 if the list is empty */
+    }
+
+    while (current)
+    {
+        printf("[%p] %d\n", (void *)current, current->n); /* Print address and value */
+        node_count++; /* Increment the node count */
+
+        /* Check for a loop by comparing the current pointer with subsequent nodes */
+        if (current <= current->next)
+        {
+            printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+            exit(98); /* Exit with code 98 if a loop is detected */
+        }
+
+        current = current->next; /* Move to the next node */
+    }
+
+    return (node_count); /* Return the number of nodes in the list */
+}
