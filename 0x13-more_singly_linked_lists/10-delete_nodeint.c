df7c846294ce464a09e76aa1d6b7c1de2fc50810
G33GII <gazasolomon@gmail.com>
2023-09-26 12:39:49 +0100

    few changes at task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,10 +8,9 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *node_shift, *previous_node;
+	listint_t *tmp = NULL;
+	listint_t *crrnt = *head;
 	size_t idx = 0;
-
-	node_shift = *head;
 
 	if (*head == NULL)
 	{
@@ -21,26 +20,28 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	/* Special case: Delete the first node (index 0) */
 	if (index == 0)
 	{
-		*head = node_shift->next; /* Update the head to the next node */
-		free(node_shift);         /* Free the deleted node */
+		*head = (*head)->next; /* Update the head to the next node */
+		free(tmp);         /* Free the deleted node */
 		return (1);
 	}
 
 	/* Traverse the list to find the node before the desired index */
 	while (idx < index - 1)
 	{
-		if (node_shift == NULL || node_shift->next == NULL)
+		if (tmp == NULL || tmp->next == NULL)
 		{
 			return (-1); /* Return -1 if the index is out of bounds */
 		}
-		previous_node = node_shift;
-		node_shift = node_shift->next;
+		/* crrnt = tmp; */
+		tmp = tmp->next;
 		idx++;
 	}
 
 	/* Update the previous node's next pointer */
-	previous_node->next = node_shift->next;
-	free(node_shift);                      /* Free the deleted node */
+
+	crrnt = tmp->next;
+	tmp->next = crrnt->next;
+	free(tmp);                      /* Free the deleted node */
 
 	return (1);
 }
