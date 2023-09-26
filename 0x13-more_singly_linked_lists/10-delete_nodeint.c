97548581fedeb9bbaa28432cf61a83867d50c575
G33GII <gazasolomon@gmail.com>
2023-09-26 13:41:08 +0100

    modified:   0x13-more_singly_linked_lists/a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	/* Traverse the list to find the node before the desired index */
-	while (idx < index - 1)
+	while (idx < index)
 	{
 		if (node_shift == NULL || node_shift->next == NULL)
 		{
