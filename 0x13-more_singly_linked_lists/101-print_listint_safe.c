086bbce1eed42dbff1218d534f9aeb2e76422dac
G33GII <gazasolomon@gmail.com>
2023-09-27 22:11:08 +0100

    changes to 101 print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,30 +7,30 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t node_count = 0; /* Initialize a counter for the number of nodes */
-    const listint_t *current = head; /* Create a current pointer for traversal */
+	size_t node_count = 0; /* Initialize a counter for the number of nodes */
+	const listint_t *current = head; /* Create a current pointer for traversal */
 
-    /* Check if the list is empty */
-    if (!head)
-    
-    {
-        exit(98); /* Exit with code 98 if the list is empty */
-    }
+	/* Check if the list is empty */
+	if (!head)
 
-    while (current)
-    {
-        printf("[%p] %d\n", (void *)current, current->n); /* Print address and value */
-        node_count++; /* Increment the node count */
+	{
+		exit(98); /* Exit with code 98 if the list is empty */
+	}
 
-        /* Check for a loop by comparing the current pointer with subsequent nodes */
-        if (current <= current->next)
-        {
-            printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-            exit(98); /* Exit with code 98 if a loop is detected */
-        }
+	while (current)
+	{                       /* Print address and value */
+		printf("[%p] %d\n", (void *)current, current->n);
+		node_count++; /* Increment the node count */
 
-        current = current->next; /* Move to the next node */
-    }
+		/* Check for a loop by comparing the current pointer with subsequent nodes */
+		if (current <= current->next)
+		{
+			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			return (node_count);
+		}
 
-    return (node_count); /* Return the number of nodes in the list */
+		current = current->next; /* Move to the next node */
+	}
+
+	return (node_count); /* Return the number of nodes in the list */
 }
