f93bb0782e2d672f5fadb10fcfc990655af19cfe
G33GII <gazasolomon@gmail.com>
2023-10-03 00:49:20 +0100

    append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,33 +8,33 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    ssize_t fileDescriptor, writeStatus;
-    int length;
+	ssize_t fileDescriptor, writeStatus;
+	int length;
 
-    /* Check if filename is NULL */
-    if (filename == NULL)
-        return (-1);
+	/* Check if filename is NULL */
+	if (filename == NULL)
+		return (-1);
 
-    /* Calculate the length of text_content */
-    if (text_content != NULL)
-    {
-        length = 0;
-        while (text_content[length])
-            length++;
-    }
+	/* Calculate the length of text_content */
+	if (text_content != NULL)
+	{
+		length = 0;
+		while (text_content[length])
+			length++;
+	}
 
-    /* Open the file in write and append mode */
-    fileDescriptor = open(filename, O_WRONLY | O_APPEND);
-    /* Write the content to the file */
-    writeStatus = write(fileDescriptor, text_content, length);
+	/* Open the file in write and append mode */
+	fileDescriptor = open(filename, O_WRONLY | O_APPEND);
+	/* Write the content to the file */
+	writeStatus = write(fileDescriptor, text_content, length);
 
-    /* Check if the file was opened and written to successfully */
-    if (fileDescriptor == -1 || writeStatus == -1)
-    {
-        return (-1);
-    }
+	/* Check if the file was opened and written to successfully */
+	if (fileDescriptor == -1 || writeStatus == -1)
+	{
+		return (-1);
+	}
 
-    /* Close the file */
-    close(fileDescriptor);
-    return (1);
+	/* Close the file */
+	close(fileDescriptor);
+	return (1);
 }
