9a9cfc11d42598ad37d9f53beb4950686bd105b9
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-09 14:47:57 +0000

    battied tas 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,35 +1,36 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - function that adds a new node at the end of a dlistint_t list.
+ * add_dnodeint_end - function that adds a new node at
+ *                                          the end of a dlistint_t list.
  * @head: ptr to the head of the doubly linked list
  * @n: value to be added to the new node
  * Return: ptr to the newly added node or NULL if it fails
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *_nw = NULL;
-    dlistint_t *_plhld = *head;
+	dlistint_t *_nw = NULL;
+	dlistint_t *_plhld = *head;
 
-    _nw = malloc(sizeof(dlistint_t));
-    if (_nw == NULL)
-        return (NULL);
+	_nw = malloc(sizeof(dlistint_t));
+	if (_nw == NULL)
+		return (NULL);
 
-    _nw->n = n;
-    _nw->next = NULL;
+	_nw->n = n;
+	_nw->next = NULL;
 
-    if ((*head) == NULL)
-    {
-        *head = _nw;
-    }
-    else
-    {
-        while ((_plhld)->next)
-            _plhld = _plhld->next;
+	if ((*head) == NULL)
+	{
+		*head = _nw;
+	}
+	else
+	{
+		while ((_plhld)->next)
+			_plhld = _plhld->next;
 
-        _plhld->next = _nw;
-        _nw->prev = _plhld;
-    }
+		_plhld->next = _nw;
+		_nw->prev = _plhld;
+	}
 
-    return (_nw);
+	return (_nw);
 }
