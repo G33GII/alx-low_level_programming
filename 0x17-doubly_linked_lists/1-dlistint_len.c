3ee92a4ad0d953dd730f8776292e435c5c8315cd
ga_za <133493252+G33GII@users.noreply.github.com>
2023-11-09 13:33:26 +0000

    update task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * dlistint_len - check the code
+ * @h: ptr to the head of the doubly linked list
  * Return: Always EXIT_SUCCESS.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int x = 0;
+	int x = 0;
 
-    for (; h; h = h->next)
-        x++;
+	for (; h; h = h->next)
+		x++;
 
-    return (x);
+	return (x);
 
 }
