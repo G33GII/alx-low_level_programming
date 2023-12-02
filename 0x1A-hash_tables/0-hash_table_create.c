b952e496027e576124657be6e763ee7dd01b8cf4
G33GII <gazasolomon@gmail.com>
2023-12-02 15:33:38 +0100

    uodate to hash table task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,23 @@ i
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *_nt;
-    unsigned long int x = 0;
+	hash_table_t *_nt;
+	unsigned long int x = 0;
 
-    _nt = malloc(sizeof(hash_table_t));
-    if (!_nt)
-        return (NULL);
+	_nt = malloc(sizeof(hash_table_t));
+	if (!_nt)
+		return (NULL);
 
-    _nt->size = size;
-    _nt->array = malloc(sizeof(hash_node_t *) * size);
-    if (!_nt->array)
-    {
-        free(_nt);
-        return (NULL);
-    }
+	_nt->size = size;
+	_nt->array = malloc(sizeof(hash_node_t *) * size);
+	if (!_nt->array)
+	{
+		free(_nt);
+		return (NULL);
+	}
 
-    for (x = 0; x < size; x++)
-        _nt->array[x] = NULL;
+	for (x = 0; x < size; x++)
+		_nt->array[x] = NULL;
 
-    return (_nt);
+	return (_nt);
 }
