98cac99021b3ec5ba308a1d62c26122968ff632b
Shekwogaza <gazasolomon@gmail.com>
2023-12-03 07:22:55 -0800

    fixed the dict output format

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,37 +7,37 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int x = 0;
-    hash_node_t *_p;
-    char _bff[1024];
+	unsigned long int x = 0, _l;
+	hash_node_t *_p;
+	char _bff[1024];
 
-    if (ht->array == NULL)
-    {
-        printf("{}\n");
-        return;
-    }
+	if (ht->array == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
 
-    for (x = 0; x < ht->size; x++)
-    {
-        _p = ht->array[x];
-        if (_p == NULL)
-            continue;
+	for (x = 0; x < ht->size; x++)
+	{
+		_p = ht->array[x];
+		if (_p == NULL)
+			continue;
 
-        while (_p)
-        {
-            strcat(_bff, "'");
-            strcat(_bff, _p->key);
-            strcat(_bff, "': '");
-            strcat(_bff, _p->value);
-            strcat(_bff, "'");
-            strcat(_bff, ", ");
-            _p = _p->next;
-        }
-    }
-    strcat(_bff, "\0");
-    _l = strlen(_bff);
-    if (_l >= 2)
-        _bff[_l - 2] = '\0';
-    strcat(_bff, "\0");
-    printf("{%s}\n", _bff);
+		while (_p)
+		{
+			strcat(_bff, "'");
+			strcat(_bff, _p->key);
+			strcat(_bff, "': '");
+			strcat(_bff, _p->value);
+			strcat(_bff, "'");
+			strcat(_bff, ", ");
+			_p = _p->next;
+		}
+	}
+	strcat(_bff, "\0");
+	_l = strlen(_bff);
+	if (_l >= 2)
+		_bff[_l - 2] = '\0';
+	strcat(_bff, "\0");
+	printf("{%s}\n", _bff);
 }
