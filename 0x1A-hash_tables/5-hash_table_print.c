f36e8f626afea00735302134740c74f03b8240e3
G33GII <gazasolomon@gmail.com>
2023-12-03 15:41:47 +0100

    update

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: is the hash table you want to look into.
+ * Return: Always EXIT_SUCCESS.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int x = 0;
+    hash_node_t *_p;
+    char _bff[1024];
+
+    if (ht->array == NULL)
+    {
+        printf("{}\n");
+        return;
+    }
+
+    for (x = 0; x < ht->size; x++)
+    {
+        _p = ht->array[x];
+        if (_p == NULL)
+            continue;
+
+        while (_p)
+        {
+            strcat(_bff, "'");
+            strcat(_bff, _p->key);
+            strcat(_bff, "': '");
+            strcat(_bff, _p->value);
+            strcat(_bff, "'");
+            strcat(_bff, ", ");
+            _p = _p->next;
+        }
+    }
+    strcat(_bff, "\0");
+    printf("{%s}\n", _bff);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,6 +41,8 @@ int replace_iffound(hash_node_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+void hash_table_print(const hash_table_t *ht);
+
 
 
 #endif /* Include Guard */
