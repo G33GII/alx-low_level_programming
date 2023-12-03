9a7b0c887bca6a578805489eb03577848b7845ac
G33GII <gazasolomon@gmail.com>
2023-12-03 14:35:59 +0100

    bettied the hash table get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,21 +8,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *_p;
-    unsigned long int index;
+	hash_node_t *_p;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL)
-        return (NULL);
-    
-    index = key_index((const unsigned char *)key, ht->size);
-    _p = ht->array[index];
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-    while (_p)
-    {
-        if (!(strcmp(_p->key, key)))
-            return (_p->value);
+	index = key_index((const unsigned char *)key, ht->size);
+	_p = ht->array[index];
 
-        _p = _p->next;
-    }
-    return (NULL);
+	while (_p)
+	{
+		if (!(strcmp(_p->key, key)))
+			return (_p->value);
+
+		_p = _p->next;
+	}
+	return (NULL);
 }
