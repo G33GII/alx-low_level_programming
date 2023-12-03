a4bbcd959fdc636ee3ef626c1f15ee3fc8976fe8
G33GII <gazasolomon@gmail.com>
2023-12-03 21:00:42 +0100

    update to advance task applying PHP sorted hash table style

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,69 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_delete - Freeing hash table
+ * @ht: Hash Table
+ * Return: void.
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+
+}
+
+
+/**
+ * shash_table_create - Creates table.
+ * @size: Size of theh hash table.
+ * Return: shash_table_t.
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+
+}
+
+
+/**
+ * shash_table_set - setting the key and values unto a hash table.
+ * @ht: Hash table.
+ * @key:
+ * @value:
+ * Return: Integer(INT).
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+
+}
+
+
+/**
+ * shash_table_get - 
+ * @ht:
+ * @key:
+ * Return: Character pointer(char *).
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+
+}
+
+
+/**
+ * shash_table_print - 
+ * @ht:
+ * Return: void.
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+
+}
+
+
+/**
+ * shash_table_print_rev - 
+ * @ht:
+ * Return: void.
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+
+}
