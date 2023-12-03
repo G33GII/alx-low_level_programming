cd47f1d578d24d7577c6828a47c9dc403b8c8900
G33GII <gazasolomon@gmail.com>
2023-12-03 20:25:16 +0100

    update on delete hash table file

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,31 +7,31 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int x = 0;
-    hash_node_t *_p, *_s;
+	unsigned long int x = 0;
+	hash_node_t *_p, *_s;
 
-    if (!(ht->array))
-    {
-        return;
-    }
+	if (!(ht->array))
+	{
+		return;
+	}
 
 	for (x = 0; x < ht->size; x++)
 	{
 		_p = ht->array[x];
 		if (_p == NULL)
-        {
+		{
 			continue;
-        }
+		}
 
 		while (_p)
 		{
-            _s = _p->next;
-            free(_p->key);
-            free(_p->value);
-            free(_p);
+			_s = _p->next;
+			free(_p->key);
+			free(_p->value);
+			free(_p);
 			_p = _s;
 		}
 	}
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
