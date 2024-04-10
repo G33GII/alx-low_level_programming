226b26b5faf88cc87174d839a556286956864548
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-10 09:10:26 +0000

    Wed, Apr 10, 2024, 10:10 AM +01:00

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search:
+ * @array:
+ * @size:
+ * @value:
+ *
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (array == NULL)
+    {
+        return -1;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Comparing %d with %d\n", array[i], value);
+        if (array[i] == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
