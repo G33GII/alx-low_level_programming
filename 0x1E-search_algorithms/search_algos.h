4025dc2db339004981f7161e19f7ea82a2468dc3
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-10 19:52:52 +0100

    update to binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,7 @@
 /* Function prototype for linear search */
 int linear_search(int *array, size_t size, int value);
 
+/* Function prototype for Binary search */
+int binary_search(int *array, size_t size, int value);
+
 #endif /* SEARCH_ALGOS_H */
