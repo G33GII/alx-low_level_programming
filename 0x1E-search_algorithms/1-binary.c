6fff8e5c1c675b2e5b3bf99b0b157dd151ee67ac
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-10 23:06:02 +0100

    bug fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -28,7 +28,8 @@ int binary_search(int *array, size_t size, int value)
 		{
             if (i == right)
                 printf("%d", array[i]);
-            printf("%d, ", array[i]);
+			else
+            	printf("%d, ", array[i]);
 		}
 		printf("\n");
 
